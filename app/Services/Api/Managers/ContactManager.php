2f83c4c3917dfef6d6ddb1c80eb06c121776dc46
wangqinwei <sin.wang@patpat.com>
2021-01-08 11:11:39 +0800

    update

--- app/Services/Api/Managers/ContactManager.php
+++ app/Services/Api/Managers/ContactManager.php
@@ -28,7 +28,7 @@ class ContactManager
     {
         Mail::send('emails.reminder', ['params' => $params], function ($m) use ($params) {
             $m->from(env('MAIL_USERNAME'));
-            $m->to('peterwusg@gmail.com')->subject($params['name']);
+            $m->to('inquiry@waterlandcap.com')->subject($params['name']);
         });
         return $this->customerService->contact($params);
     }
