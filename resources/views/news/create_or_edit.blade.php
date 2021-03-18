b11ff899c5496a774ede15e0ab99b925237e3280
wangqinwei <sin.wang@patpat.com>
2021-03-18 20:00:07 +0800

    udpate

--- resources/views/news/create_or_edit.blade.php
+++ resources/views/news/create_or_edit.blade.php
@@ -39,10 +39,7 @@
                                     </select>
                                 </div>
                                
-                                <div class="form-group">
-                                    <label for="">标签</label>
-                                    <input type="text" name="tag" value="{{$news->tag or ''}}" class="form-control" id="exampleInputEmail1" placeholder="">
-                                </div>
+                          
 
                                 <div class="form-group">
                                     <label for="">描述</label>
